5f594ff6904ab42988349e4498bdd7e1d57e9180
Faisal Sayyed <sayyedfaisal06@gmail.com>
2024-10-12 21:53:42 +0530

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://polished-koala-86.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
